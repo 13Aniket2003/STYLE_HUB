53f77b82c85361147f714b3aae503d7201f5e5cc
13Aniket2003 <duttaaniket637@gmail.com>
2026-02-21 17:28:16 +0530

    all file saved

--- stylehub/stylehub/settings.py
+++ stylehub/stylehub/settings.py
@@ -207,4 +207,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
